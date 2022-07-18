597893f44032c28a082565d344d579ff1ed92598
Nikunj Nandaniya <nikunjit007az204@outlook.com>
2022-07-18 21:14:52 +0530

    Add project files.

---
+++ Services.ProductAPI/DbContexts/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Services.ProductAPI.Models;
+
+namespace Services.ProductAPI.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+         : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
